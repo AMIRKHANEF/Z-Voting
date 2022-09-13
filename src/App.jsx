36cd5514f45e167a14ca8e7e10efd534d8993211
Amir Ekbatani Fard <75857984+AMIRKHANEF@users.noreply.github.com>
2022-09-13 15:25:22 +0430

    icons added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Grid,Button, Typography } from "@mui/material";
+import {HowToVote as HowToVoteIcon, ThumbsUpDown as ThumbsUpDownIcon, Ballot as BallotIcon} from '@mui/icons-material';
 import React, { useState } from 'react';
 import {Voter} from './components/voterPage'
 import {InitiateVoting} from './components/InitiateVoting'
@@ -14,12 +15,14 @@ function Voting() {
             <Grid item xs={12} textAlign='center' sx={{py:5, fontWeight:700, fontSize:25}}>
               <Typography color={'black'} variant={'h3'} fontWeight={700} p={0} >Welcome to Zero-Knowledge & Gasless Voting framework </Typography>
             </Grid>
-            <Grid item container justifyContent={'center'} alignItems={'center'} pt={8} spacing={15}>
-              <Grid item><Button sx={{ width: '210px', height: '120px', fontWeight: 600, fontSize: '18px'}} variant="contained" onClick={()=> setChoose(0)}>Initiate a voting</Button></Grid>
-              <Grid item><Button sx={{ width: '210px', height: '120px', fontWeight: 600, fontSize: '18px'}} variant="contained" onClick={()=> setChoose(1)}>Go vote</Button></Grid>
+            <Grid item container justifyContent={'center'} alignItems={'center'} pt={8} >
+              <Grid item><Button color="success" startIcon={<HowToVoteIcon style={{fontSize: '40px'}} />} sx={{ width: '310px', height: '80px', fontWeight: 600, fontSize: '18px'}} variant="contained" onClick={()=> setChoose(0)}>Initiate a voting</Button></Grid>
             </Grid>
-            <Grid item container justifyContent={'center'} alignItems={'center'} pt={15} spacing={15}>
-              <Grid item><Button sx={{ width: '310px', height: '80px', fontWeight: 600, fontSize: '18px'}} variant="contained" onClick={()=> setChoose(2)}>generate votingID</Button></Grid>
+            <Grid item container justifyContent={'center'} alignItems={'center'} pt={2}>
+              <Grid item><Button startIcon={<ThumbsUpDownIcon style={{fontSize: '40px'}} />} sx={{ width: '310px', height: '80px', fontWeight: 600, fontSize: '18px'}} variant="contained" onClick={()=> setChoose(1)}>Go vote</Button></Grid>
+            </Grid>
+            <Grid item container justifyContent={'center'} alignItems={'center'} pt={2} >
+              <Grid item><Button color="warning" startIcon={<BallotIcon style={{fontSize: '40px'}} />} sx={{ width: '310px', height: '80px', fontWeight: 600, fontSize: '18px'}} variant="contained" onClick={()=> setChoose(2)}>generate votingID</Button></Grid>
             </Grid>
           </>
         }
